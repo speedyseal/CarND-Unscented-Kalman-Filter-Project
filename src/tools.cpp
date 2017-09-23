7abb6b26b0a8bae27d9aebf1fffcfe005e110b75
Speedy Seal <speedyseal@github.com>
2017-09-22 18:45:18 -0700

    First changes

--- src/tools.cpp
+++ src/tools.cpp
@@ -12,7 +12,29 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
-  TODO:
+
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+  rmse << 0., 0., 0., 0.;
+
+  if(estimations.size() != ground_truth.size()) {
+    cout << "error: estimations != ground_truth dimentions" << endl;
+    return rmse;
+  }
+  if( estimations.size() == 0 ) {
+    cout << "error: estimations size == 0" << endl;
+    return rmse;
+  }
+  
+  for(auto i=0; i< estimations.size(); ++i) {
+    VectorXd tmp = estimations[i] - ground_truth[i];
+    tmp = tmp.array() * tmp.array();
+    rmse = rmse + tmp;
+  }
+
+  rmse = rmse / estimations.size();
+  rmse = rmse.array().sqrt();
+
+  return rmse;
 }
--- src/ukf.cpp
+++ src/ukf.cpp
@@ -11,17 +11,13 @@ using std::vector;
  * Initializes Unscented Kalman filter
  */
 UKF::UKF() {
+  is_initialized_ = false;
+  
   // if this is false, laser measurements will be ignored (except during init)
   use_laser_ = true;
 
   // if this is false, radar measurements will be ignored (except during init)
   use_radar_ = true;
-
-  // initial state vector
-  x_ = VectorXd(5);
-
-  // initial covariance matrix
-  P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
   std_a_ = 30;
@@ -44,6 +40,7 @@ UKF::UKF() {
   // Radar measurement noise standard deviation radius change in m/s
   std_radrd_ = 0.3;
 
+
   /**
   TODO:
 
@@ -51,6 +48,29 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+
+  time_us_ = 0;
+
+  n_x_ = 5;
+
+  n_aug_ = 7;
+
+  lambda_ = 3-n_aug_;
+
+  // initial state vector
+  x_ = VectorXd(n_aug_);
+  x_.fill(0.0);
+
+  // initial covariance matrix
+  P_ = MatrixXd(n_aug_, n_aug_);
+  P_ = MatrixXd::Identity(n_aug_, n_aug_);
+
+  // predicted sigma points matrix
+  Xsig_pred_ = MatrixXd(n_x_, 2*n_x_+1);
+
+  // weights of sigma points
+  weights_ = VectorXd(2*n_aug_+1);
+
 }
 
 UKF::~UKF() {}
@@ -59,12 +79,82 @@ UKF::~UKF() {}
  * @param {MeasurementPackage} meas_package The latest measurement data of
  * either radar or laser.
  */
-void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
+void UKF::ProcessMeasurement(MeasurementPackage measurement_pack) {
   /**
   TODO:
 
   Complete this function! Make sure you switch between lidar and radar
   measurements.
+  */
+
+  /*****************************************************************************
+   *  Initialization
+   ****************************************************************************/
+  if (!is_initialized_) {
+    // first measurement
+    if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+      /**
+      Convert radar from polar to cartesian coordinates and initialize state.
+      */
+      const auto ro = measurement_pack.raw_measurements_(0);
+      const auto phi = measurement_pack.raw_measurements_(1);
+      const auto x = cos(phi) * ro;
+      const auto y = sin(phi) * ro;
+      x_ << x, y, 0., 0., 0.;
+    }
+    else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
+      /**
+      Initialize state.
+      */
+      const auto x = measurement_pack.raw_measurements_(0);
+      const auto y = measurement_pack.raw_measurements_(1);
+      x_ << x, y, 0., 0., 0.;
+    }
+
+    time_us_ = measurement_pack.timestamp_;
+
+    // done initializing, no need to predict or update
+    is_initialized_ = true;
+
+    /*
+    // extra debugging prints
+    cout << "x_ = " << x_ << endl;
+    cout << "P_ = " << P_ << endl;
+    */
+    return;
+  }
+
+  /*****************************************************************************
+   *  Prediction
+   ****************************************************************************/
+
+  /**
+   TODO:
+     * Update the state transition matrix F according to the new elapsed time.
+      - Time is measured in seconds.
+     * Update the process noise covariance matrix.
+     * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
+   */
+  const auto dt = (measurement_pack.timestamp_ - time_us_) / 1000000.0;
+  time_us_ = measurement_pack.timestamp_;
+
+  Prediction(dt);
+
+  /*****************************************************************************
+   *  Update
+   ****************************************************************************/
+
+  if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+    UpdateRadar(measurement_pack);
+  } else {
+    // Laser updates
+    UpdateLidar(measurement_pack);
+  }
+
+  /*
+  // print the output
+  cout << "x_ = " << x_ << endl;
+  cout << "P_ = " << P_ << endl;
   */
 }
 
@@ -80,6 +170,11 @@ void UKF::Prediction(double delta_t) {
   Complete this function! Estimate the object's location. Modify the state
   vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
+  // generate sigma points
+
+  // predict sigma points
+
+  // predict mean state and covariance
 }
 
 /**
